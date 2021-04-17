5a49f49f4ce9b099385c002853318f7c62c3590d
Leonardo Anthony <67436042+leonardoanthony@users.noreply.github.com>
2021-04-16 21:12:35 -0300

    JAVASCRIPT 02
    Tipos de Dados:
    - String
    - Number
    - Boolean
    - Undefined
    - Null
    - Symbol
    - Object

---
+++ javascript/mod2/0202_tipos-de-dados/script-aula.js
@@ -0,0 +1,23 @@
+// Tipos de Dados
+
+var nome = 'Leonardo';  // String
+var idade = 23;         // Number
+var estudante = true;   // Boolean
+var time;               // Undefined
+var comida = null;      // Null
+var simbolo = Symbol(); // Symbol
+var novoObjeto = {};    // Object;
+
+
+// Verificar o tipo do dado
+console.log(typeof nome);
+
+// Concatenação
+
+var sobrenome = ' Anthony';
+var nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = 'Romario fez '  + gols + ' gols!';
+console.log(frase);
---
+++ javascript/mod2/0202_tipos-de-dados/script.js
@@ -0,0 +1,10 @@
+// Exercício
+
+var texto = 'Variavel com string';
+var numeroTexto = '21';
+var idade = 22;
+var nome = 'Leonardo';
+var sobrenome = 'Anthony';
+var nomeCompleto = `${nome} ${sobrenome }`;
+var frase = "It's time";
+console.log(typeof nome);
