997de0763f9f7326e84d178904a8c2b87cb9c020
Leonardo Anthony <67436042+leonardoanthony@users.noreply.github.com>
2021-05-02 16:04:19 -0300

    JAVASCRIPT 10
    - Escopo
    - Var, Const e Let

---
+++ javascript/mod2/10_escopo/script-aula.js
@@ -0,0 +1,30 @@
+console.log('--- Script Aula ---');
+
+if(true){
+    var carro = 'Fusca';
+    console.log(carro);
+}
+
+console.log(carro);
+
+{
+    var nome = 'Fusca';
+    const ano = '2019';
+}
+
+for(let i = 0; i<10; i++){
+    console.log(i);
+}
+
+const mes = 'DEZ';
+// mes = 'JAN';  //erro, tentou modificar
+// const semana; //erro, sem valor
+
+const data = {
+    dia:  25,
+    mes: 'Abril',
+    ano:  1998,
+}
+
+data.dia = 26;
+// data = 'Janeiro'; //ERRO
---
+++ javascript/mod2/10_escopo/script.js
@@ -0,0 +1,31 @@
+console.log('--- Script Exercício ---');
+
+
+{
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;  
+    
+    console.log(cor, marca, portas);
+}
+
+
+const dois = 2;
+
+function somarDois(x){
+    return x + dois;
+}
+
+function dividirDois(x){
+    return x / dois;
+}
+
+
+var numero = 50;
+
+for(let numero = 0;numero<10; numero++){
+    console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
