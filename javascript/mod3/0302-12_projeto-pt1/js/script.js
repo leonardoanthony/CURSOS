c28bc3876d9919c85df6cc6afb022980bde8d284
Leonardo Anthony <67436042+leonardoanthony@users.noreply.github.com>
2021-07-12 09:31:54 -0300

    JAVASCRIPT 03 - 03
    Seleção de Elementos em Javascript pt1

---
+++ javascript/mod3/0302-12_projeto-pt1/js/script.js
@@ -0,0 +1,15 @@
+const animaisSection = document.getElementById('animais');
+console.log(animaisSection);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
+
+const navItem = document.getElementsByTagName('ul');
+console.log(navItem);
+
+const animais = document.querySelector('.animais');
+console.log(animais);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg);
+
