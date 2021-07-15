6e12c74928ec93fca364f36aecde56275ff6a53c
Leonardo Anthony <67436042+leonardoanthony@users.noreply.github.com>
2021-07-15 09:31:36 -0300

    JAVASCRIPT 03 - 10
    Accordion List

--- javascript/mod3/0302-12_projeto-pt1/js/script.js
+++ javascript/mod3/0302-12_projeto-pt1/js/script.js
@@ -1,27 +1,48 @@
 // Javascript 03 - 09
 // Navegação por tabs
 
-document.documentElement.classList.add('js');
+function initTabNav(){
 
-const tabmenu = document.querySelectorAll('.js-tabmenu li');
-const tabcontent = document.querySelectorAll('.js-tabcontent section');
+    const tabmenu = document.querySelectorAll('.js-tabmenu li');
+    const tabcontent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabcontent.length && tabmenu.length){
+    if(tabcontent.length && tabmenu.length){
 
-tabcontent[0].classList.add('ativo');
+    tabcontent[0].classList.add('ativo');
 
-function activeTab(index){
-    tabcontent.forEach((item) => {
-        item.classList.remove('ativo');
-    })
-    tabcontent[index].classList.add('ativo');
-}
+    function activeTab(index){
+        tabcontent.forEach((item) => {
+            item.classList.remove('ativo');
+        })
+        tabcontent[index].classList.add('ativo');
+    }
 
 
 
-tabmenu.forEach((item, index) => {
-    item.addEventListener('click', () =>{
-        activeTab(index);
+    tabmenu.forEach((item, index) => {
+        item.addEventListener('click', () =>{
+            activeTab(index);
+        });
     });
-});
+    }
 }
+
+initTabNav();
+
+// Javascript 03 - 10
+// Accordion List
+
+const accordionList = document.querySelectorAll('.js-accordionList dt');
+const activeClass = 'ativo';
+
+accordionList[0].classList.add(activeClass);
+accordionList[0].nextElementSibling.classList.add(activeClass);
+
+function activeAccordionList(){
+    this.classList.toggle(activeClass);
+    this.nextElementSibling.classList.toggle(activeClass);
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordionList);
+});
