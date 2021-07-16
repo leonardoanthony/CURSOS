dae602c6fcc0e8c8fe6768cdfa12330a127e7b61
Leonardo Anthony <67436042+leonardoanthony@users.noreply.github.com>
2021-07-16 11:00:47 -0300

    JAVASCRIPT 04 - 03
    Objetos nativos, host e user

---
+++ javascript/mod4/0403-navive-host-e-user/script.js
@@ -0,0 +1,15 @@
+//Liste 5 Objetos Nativos
+// Object
+// String
+// Array
+// function
+// Number
+
+
+// Liste 5 objetos do browser
+
+// Window
+// history
+// Document
+// HTMLCollection
+// NodeList
